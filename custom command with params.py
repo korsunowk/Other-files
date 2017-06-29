79f00a8ddb71059c82de32a9bd83e467e7b44794
Korsunow Konstantin <korsunowk@gmail.com>
2017-06-29 10:52:00 +0300

    Update custom command with params.py

--- custom command with params.py
+++ custom command with params.py
@@ -1,5 +1,5 @@
-from django.core.mail import send_mail
 from django.core.management import BaseCommand
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils import timezone
