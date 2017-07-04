314f5f3ed143b3d18cc0e97af019e34014230780
Korsunow Konstantin <korsunowk@gmail.com>
2017-07-04 14:39:51 +0300

    Update django-aggregation.py

--- django-aggregation.py
+++ django-aggregation.py
@@ -1,1 +1,18 @@
 
+@staticmethod
+def aggregate_game_stats(game_stats):
+    """
+    Help method for aggregation game stats
+    :param game_stats: GameStats table
+    :return: dictionary with stats from GameStats table
+    """
+    return game_stats.aggregate(
+        gp=Count('game_id'),
+        won_count=Count(Case(When(won=True, then=1))),
+        loss_count=Count(Case(When(won=False, then=1))),
+        fg_attempts=Sum('fg_attempts'),
+        ft_attempts=Sum('ft_attempts'),
+        of_rebounds=Sum('of_rebounds'),
+        pts=Sum('points'),
+        turnovers=Sum('turnovers')
+    )
