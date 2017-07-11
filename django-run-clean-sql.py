b8cbe92c78bb85c65bf848b6ab92d3a2ffbf1fbd
Korsunow Konstantin <korsunowk@gmail.com>
2017-07-11 13:44:26 +0300

    Update django-run-clean-sql.py

--- django-run-clean-sql.py
+++ django-run-clean-sql.py
@@ -1,3 +1,8 @@
+from django.db import connection
+from django.conf import settings
+from django.core.mail import send_mail as django_send_mail
+from django.template import Context, Template
+
 def run_raw_sql(query_template, params):
     query_template = Template(query_template)
     query = query_template.render(Context(params))
@@ -7,3 +12,4 @@ def run_raw_sql(query_template, params):
         result = dictfetchall(cursor)
 
     return result
+
