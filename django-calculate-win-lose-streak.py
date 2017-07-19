01ab86bf4c31c784541dd26b1d85583d273ceabd
Korsunow Konstantin <korsunowk@gmail.com>
2017-07-19 11:48:11 +0300

    Update django-calculate-win-lose-streak.py

--- django-calculate-win-lose-streak.py
+++ django-calculate-win-lose-streak.py
@@ -8,5 +8,6 @@ for game in games_stats.order_by('-game__game_date'):
         win_streak.append([])
         lose_streak[-1].append(game)
 
+# get array with streak with max len
 win_streak = max(win_streak, key=lambda x: len(x))
 lose_streak = max(lose_streak, key=lambda x: len(x))
